43032cb35f70820fe75f76798f766092be06d219
Ulrik Strid <ulrik.strid@outlook.com>
2022-03-23 08:10:30 +0100

    Don't fail if not finding directory

--- lib/git.ml
+++ lib/git.ml
@@ -5,4 +5,7 @@ let clone ?(depth = 3000) repo target =
 let pull cwd = Lwt_process.exec ~cwd ("git", [| "git"; "pull" |])
 
 let clone_or_pull ?depth repo target =
-  if Sys.is_directory target then pull target else clone ?depth repo target
+  try
+    if Sys.is_directory target then pull target else clone ?depth repo target
+  with
+  | _ -> clone ?depth repo target
