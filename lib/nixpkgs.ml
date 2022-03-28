3281ff9c2ae1f7f7d839a6b03874ba4a0b991286
Ulrik Strid <ulrik.strid@outlook.com>
2022-03-28 12:05:08 +0200

    Error handling in nixpkgs

--- lib/nixpkgs.ml
+++ lib/nixpkgs.ml
@@ -1,19 +1,27 @@
+open Lwt.Syntax
+
 let src = Logs.Src.create "nixpkgs"
 
 module Log = (val Logs.src_log src : Logs.LOG)
 
 let get_version nixpkgs_path name =
   Log.debug (fun m -> m "get_version: %s" name);
-  let open Lwt.Infix in
-  Lwt_process.pread_line
-    ~cwd:nixpkgs_path
-    ( "nix"
-    , [| "nix"; "eval"; "-f"; "./"; "ocamlPackages." ^ name ^ ".version" |] )
-  >|= Astring.String.trim ~drop:(fun c -> c = '"')
+  try
+    let+ version =
+      Lwt_process.pread_line
+        ~cwd:nixpkgs_path
+        ( "nix"
+        , [| "nix"; "eval"; "-f"; "./"; "ocamlPackages." ^ name ^ ".version" |]
+        )
+    in
+    Astring.String.trim ~drop:(fun c -> c = '"') version
+  with
+  | _ ->
+    Log.err (fun m -> m "get_version failed for %s" name);
+    Lwt.return ""
 
 let get_name nixpkgs_path name =
   Log.debug (fun m -> m "get_name: %s" name);
-  let open Lwt.Syntax in
   let+ raw_pname =
     Lwt_process.pread_line
       ~cwd:nixpkgs_path
