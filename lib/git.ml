696464ab548a96f2020914d816aacd5a89171c59
Ulrik Strid <ulrik.strid@outlook.com>
2022-03-28 11:54:28 +0200

    Clean  git etc

--- lib/git.ml
+++ lib/git.ml
@@ -6,19 +6,29 @@ let clone ?(depth = 3000) repo target =
   Log.info (fun m -> m "Cloning %s into %s" repo target);
 
   Lwt_process.pread_lines
-    ~stderr:(`FD_copy Unix.stdout)
     ("git", [| "git"; "clone"; "--depth=" ^ string_of_int depth; repo; target |])
   |> Lwt_stream.iter (fun str -> Log.info (fun m -> m "%s" str))
 
 let pull cwd =
-  Lwt_process.pread_lines
-    ~stderr:(`FD_copy Unix.stdout)
-    ~cwd
-    ("git", [| "git"; "pull" |])
+  Lwt_process.pread_lines ~cwd ("git", [| "git"; "pull" |])
+  |> Lwt_stream.iter (fun str -> Log.info (fun m -> m "%s" str))
+
+let clean cwd =
+  Lwt_process.pread_lines ~cwd ("git", [| "git"; "clean"; "-f"; "-d" |])
+  |> Lwt_stream.iter (fun str -> Log.info (fun m -> m "%s" str))
+
+let restore cwd =
+  Lwt_process.pread_lines ~cwd ("git", [| "git"; "restore"; "*" |])
   |> Lwt_stream.iter (fun str -> Log.info (fun m -> m "%s" str))
 
 let clone_or_pull ?depth repo target =
+  let open Lwt.Syntax in
   try
-    if Sys.is_directory target then pull target else clone ?depth repo target
+    if Sys.is_directory target
+    then
+      let* () = clean target in
+      let* () = restore target in
+      pull target
+    else clone ?depth repo target
   with
   | _ -> clone ?depth repo target
